40ec8c733707d5922b07e9fbae48af054ffb2376
Jaskaran <write2jaskaransingh@gmail.com>
2024-08-15 00:05:56 +0530

    add Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,35 @@
+import { Router, Route } from "@solidjs/router";
+
 import styles from "./style/App.module.scss";
 import Navbar from "./Navbar";
+import Home from "./Home";
+import Test from "./Test";
+// import Display from "./Display";
+// import About from "./About";
 
-import discordIcon from "./assets/discord.svg";
-import githubIcon from "./assets/github.svg";
-import mailIcon from "./assets/mail.svg";
-
-const Intro = () => {
-  return (
-    <>
-      <h1 className={styles.title}>Hi There!</h1>
-      <h2 className={styles.subtitle}>
-        Welcome to <span className={styles.mid_link}>Drisht.ai</span>
-      </h2>
-      <p className={styles.description}>
-        A growing platform showcasing
-        <br />
-        Computer Vision applications
-      </p>
-      <p className={styles.check}>
-        Check out our work{" "}
-        <a href="/Test" className={styles.highlight}>
-          here
-        </a>
-        .
-      </p>
-    </>
-  );
-};
-
-const Contribute = () => {
-  return (
-    <div className={styles.contribute}>
-      <p>
-        Want to contribute to this platform?
-        <br />
-        Connect with us
-      </p>
-      <div className={styles.socialIcons}>
-        <a href="/">
-          <img src={githubIcon} alt="GitHub" />
-        </a>
-        <a href="/">
-          <img src={discordIcon} alt="Discord" />
-        </a>
-        <a href="/">
-          <img src={mailIcon} alt="Email" />
-        </a>
-      </div>
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div class={styles.container}>
+//       <Navbar />
+//       <div class={styles.main}>
+//         <Intro />
+//         <Contribute />
+//       </div>
+//     </div>
+//   );
+// };
 
 const App = () => {
   return (
-    <div class={styles.container}>
+    <div className={styles.container}>
       <Navbar />
-      <div class={styles.main}>
-        <Intro />
-        <Contribute />
+      <div className={styles.main}>
+        <Router>
+          <Route end component={Home} />
+          <Route path="/Test" component={Test} />
+          {/* <Route path="/Display" component={Display} /> */}
+          {/* <Route path="/About" component={About} /> */}
+        </Router>
       </div>
     </div>
   );
---
+++ src/Home.jsx
@@ -0,0 +1,61 @@
+import styles from "./style/App.module.scss";
+import discordIcon from "./assets/discord.svg";
+import githubIcon from "./assets/github.svg";
+import mailIcon from "./assets/mail.svg";
+
+const Intro = () => {
+  return (
+    <>
+      <h1 className={styles.title}>Hi There!</h1>
+      <h2 className={styles.subtitle}>
+        Welcome to <span className={styles.mid_link}>Drisht.ai</span>
+      </h2>
+      <p className={styles.description}>
+        A growing platform showcasing
+        <br />
+        Computer Vision applications
+      </p>
+      <p className={styles.check}>
+        Check out our work{" "}
+        <a href="/Test" className={styles.highlight}>
+          here
+        </a>
+        .
+      </p>
+    </>
+  );
+};
+
+const Contribute = () => {
+  return (
+    <div className={styles.contribute}>
+      <p>
+        Want to contribute to this platform?
+        <br />
+        Connect with us
+      </p>
+      <div className={styles.socialIcons}>
+        <a href="/">
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        <a href="/">
+          <img src={discordIcon} alt="Discord" />
+        </a>
+        <a href="/">
+          <img src={mailIcon} alt="Email" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const Home = () => {
+  return (
+    <>
+      <Intro />
+      <Contribute />
+    </>
+  );
+};
+
+export default Home;
