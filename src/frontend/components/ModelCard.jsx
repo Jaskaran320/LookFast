e9bfd0c82c77dde10b5c615205b6bd64d1fa16d7
adityaahuja7 <ahujaaditya00@gmail.com>
2024-08-27 23:13:34 +0530

    Fix deployement 404 issue.

--- src/frontend/components/ModelCard.jsx
+++ src/frontend/components/ModelCard.jsx
@@ -1,16 +1,14 @@
 import styles from "../stylesheets/Models.module.scss";
 
 const ModelCard = (props) => {
-  const handleClick = () => {
-    window.location.href = props.model.usageURL;
-  };
+
 
   return (
-    <div class={styles.modelCard} onClick={handleClick}>
+    <a href = "/segment" class={styles.modelCard}>
       <img src={props.model.image} alt={props.model.name} />
       <h2>{props.model.name}</h2>
       <p>{props.model.description}</p>
-    </div>
+    </a>
   );
 };
 
