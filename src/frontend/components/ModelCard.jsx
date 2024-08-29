6e47aae486937d1d5df7b8c52b75f61dec6b5fef
Jaskaran <write2jaskaransingh@gmail.com>
2024-08-29 22:30:20 +0530

    test window api

--- src/frontend/components/ModelCard.jsx
+++ src/frontend/components/ModelCard.jsx
@@ -1,12 +1,16 @@
 import styles from "../stylesheets/Models.module.scss";
 
 const ModelCard = (props) => {
+  const handleClick = () => {
+    window.location.href = props.model.usageURL;
+  };
+
   return (
-    <a href="/segment" class={styles.modelCard}>
+    <div class={styles.modelCard} onClick={handleClick}>
       <img src={props.model.image} alt={props.model.name} />
       <h2>{props.model.name}</h2>
       <p>{props.model.description}</p>
-    </a>
+    </div>
   );
 };
 
